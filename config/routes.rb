58338051a56367e65592ecd3c91cdd870ec3bac4
Simon Rai <simon@raize.ca>
2014-04-25 21:45:02 +0000

    added user authentication (signup, login, edit)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'pages/about'
 
   get 'pages/contact'
+
+  get 'pages/index'
+
+  root 'pages#index'
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
